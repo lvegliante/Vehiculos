36c21ff3833ad26f202dc7a9f1bac1ad4dc73dcc
LUIS ALFREDO VEGLIANTE PALMERA <luisvegliante147779@correo.itm.edu.co>
2021-08-25 23:12:01 -0500

    Entidad Vehiculos

---
+++ Vehiculos.API/Data/Entities/VehiculoType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehiculos.API.Data.Entities
+{
+    public class VehiculoType
+    {
+       public int Id { get; set; }
+        [Display(Name = "Tipo de Vehiculo")]
+        [MaxLength(50, ErrorMessage = "El Campo {0} No Puede Tener Mas De {1} Caracteres.")]
+        [Required(ErrorMessage = "El Campo {0} es Obligatorio")]
+       public string Description { get; set; }
+    }
+}
